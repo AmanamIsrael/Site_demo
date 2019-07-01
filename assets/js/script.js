c5b971324eeece9f23d05c2ab30a6fc05c40202f
Emmanuel John <Emmanuelhashy@gmail.com>
2019-07-01 19:00:18 +0100

    testing responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 // scripts go here
+
+let mainNav = document.getElementById("js-menu");
+    let navBarToggle = document.getElementById("js-navbar-toggle");
+
+    navBarToggle.addEventListener("click", function() {
+    mainNav.classList.toggle("active");
+});
+
